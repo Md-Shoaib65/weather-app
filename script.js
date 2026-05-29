8833f68ae984628e484021fd0b44836e4706d43a
Mohammad Shoaib <smd83666@gmail.com>
2026-05-29 13:23:53 +0530

    review updates

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const button = document.createElement("btn");
-// Working
+const apiKey = "2a614d17c8ba5ff342fffcc438527034"
+const weatherDataEle = document.querySelector(".weather-data")
+const cityNameEle = document.querySelector("#city-name")
+const formEle = document.querySelector("form")
+const imgIcon = document.querySelector(".icon")
+
+formEle.addEventListener("sumbit", ()=>{
+    console.log(cityNameEle.value);
+    // const cityValue = cityNameEle.value
+
+    // getWeatherData(cityValue)
+})
+
+
+// function getWeatherData(){}
