bd979c271eeb923b76823f01facde5d75b8df330
Md-Shoaib65 <smd83666@gmail.com>
2026-06-01 19:53:13 +0530

    dev (#1)
    * dev
    
    * review updates
    
    * review uupdates 2
    
    * review uupdates 3
    
    * error handling & data consumption
    
    * Update script.js
    
    * fixed detail issue
    
    * Fix argument and comma issues
    
    * code cleanup
    
    ---------
    
    Co-authored-by: Harshit <105835098+Zack-Dx@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-const button = document.createElement("btn");
+const apiKey = "8d4d47e3f3e41e9559fac12656d7a595"
+
+const weatherDataEle = document.querySelector("#weather-data")
+const cityNameEle = document.querySelector("#city-name")
+const formEle = document.querySelector("form")
+const imgIcon = document.querySelector(".icon")  
+
+formEle.addEventListener("submit", (e)=>{
+    e.preventDefault()                           
+    const cityValue = cityNameEle.value
+
+    getWeatherData(cityValue)
+})
+
+async function getWeatherData(cityValue){
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`)
+        if(!response.ok){
+            throw new Error("Network response is not ok!")
+        }
+        const data = await response.json()
+
+        const temperature = Math.floor(data.main.temp)        
+        const description = data.weather[0].description
+        const icon = data.weather[0].icon
+
+        const details = [                                      
+            `Feels Like: ${Math.floor(data.main.feels_like)}°C`,
+`Humidity: ${data.main.humidity}%`,
+            `Wind Speed: ${data.wind.speed} m/s`
+        ]
+
+        weatherDataEle.querySelector(".temp").textContent = `${temperature}°C`
+        weatherDataEle.querySelector(".desc").textContent = `${description}`
+
+        imgIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}.png" alt="">`
+
+          weatherDataEle.querySelector(".details").innerHTML = details.map((detail)=>{
+            return `<div>${detail}<div>`
+        }).join("")
+      
+
+    }catch(err){
+        weatherDataEle.querySelector(".temp").textContent = ""
+        imgIcon.innerHTML = ""
+        weatherDataEle.querySelector(".desc").textContent = "An Error Occured!"
+    }
+}
