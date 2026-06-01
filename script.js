d321f63bdc4e4810be01108d8a94065d5c0e7fc6
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2026-06-01 19:02:43 +0530

    code cleanup

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const apiKey = "8d4d47e3f3e41e9559fac12656d7a595"
 const weatherDataEle = document.querySelector("#weather-data")
 const cityNameEle = document.querySelector("#city-name")
 const formEle = document.querySelector("form")
-const imgIcon = document.querySelector(".icon")   // Later, I will fetch data from the OpenWeatherMap API using JavaScript. Based on that data, I will dynamically change the background or weather image to match the current weather conditions.
+const imgIcon = document.querySelector(".icon")  
 
 formEle.addEventListener("submit", (e)=>{
-    e.preventDefault()                            // (e)-> event To stop a form from submitting and reloading the page by default (behaviour of event)
-    // console.log(cityNameEle.value);
+    e.preventDefault()                           
     const cityValue = cityNameEle.value
 
     getWeatherData(cityValue)
@@ -20,13 +19,12 @@ async function getWeatherData(cityValue){
             throw new Error("Network response is not ok!")
         }
         const data = await response.json()
-        // console.log(data);
 
-        const temperature = Math.floor(data.main.temp)         // all accessed from json object
+        const temperature = Math.floor(data.main.temp)        
         const description = data.weather[0].description
         const icon = data.weather[0].icon
 
-        const details = [                                      // creating array to access all in place from json object
+        const details = [                                      
             `Feels Like: ${Math.floor(data.main.feels_like)}°C`,
 `Humidity: ${data.main.humidity}%`,
             `Wind Speed: ${data.wind.speed} m/s`
